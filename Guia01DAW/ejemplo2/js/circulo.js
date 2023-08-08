f06a8c23893da6fe5037465be5a443598f8ea116
Carlos Pena <carlos.cpa26@gmail.com>
2023-08-08 17:12:24 -0600

    agregando codigo ejemplo 2

---
+++ Guia01DAW/ejemplo2/js/circulo.js
@@ -0,0 +1,5 @@
+const PI = 3.1415926535;
+var radio = prompt('Introduzca el radio del círculo:','');
+var area;
+area = PI*radio*radio;
+document.write("<header><h1>El área del círculo es: " + area + "</h1><hr /><br /></header>");
---
+++ Guia01DAW/ejemplo2/js/triangulo.js
@@ -0,0 +1,6 @@
+let base = prompt("Introduzca el valor de la base:","");
+let altura = prompt("Introduzca el valor de la altura:","");
+
+
+let area = (base * altura)/2;
+document.write("<header><h1>El área del triangulo es: " + area + "</h1><hr /><br /></header>");
